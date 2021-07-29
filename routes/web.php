f409d9feb45c0aa738479ce9f6b68415d524e70e
mani4578 <hunter.alone444@gmail.com>
2021-07-29 15:04:25 +0500

    Setting blade

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+// Route::get('/', function () {
+//     return view('single-page');
+// });
+Route::get('home', function () {
+    return view('index');
+});
+Route::get('contact', function () {
+    return view('contact');
+});
+Route::get('single', function () {
     return view('single-page');
 });
 
