c24d9fee67d16d2a880a9e5476482fec82b937c9
Muhammad Bilal Anwar <b.anwar21.a@gmail.com>
2021-07-29 17:12:16 +0500

    route

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('single-page');
 // });
-Route::get('home', function () {
+Route::get('home1', function () {
     return view('index');
 });
 Route::get('contact', function () {
