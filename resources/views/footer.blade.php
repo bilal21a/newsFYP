b1bb515e5b22963ac5fabc13bdd7fd37ad34c8bb
mani4578 <hunter.alone444@gmail.com>
2021-08-02 14:39:10 +0500

    footer and header

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,33 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>News 24 - Free News Website Templates</title>
-        <meta content="width=device-width, initial-scale=1.0" name="viewport">
-        <meta content="Bootstrap Ecommerce Template" name="keywords">
-        <meta content="Bootstrap Ecommerce Template Free Download" name="description">
-        <!-- Favicon -->
-        <!-- <link href="img/favicon.ico" rel="icon"> -->
-        <link href="{{asset('img/favicon.ico')}}">
 
-
-        <!-- Google Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&display=swap" rel="stylesheet">
-
-        <!-- CSS Libraries -->
-        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-        <!-- <link href="lib/slick/slick.css" rel="stylesheet"> -->
-        <link href="{{asset('lib/slick/slick.css')}}" rel="stylesheet">
-        <link href="{{asset('lib/slick/slick-theme.css')}}" rel="stylesheet">
-
-
-        <!-- Template Stylesheet -->
-        <link href="{{asset('css/style.css')}}" rel="stylesheet">
-
-    </head>
-
- <body>
  <!-- Footer Start -->
         <div class="footer">
             <div class="container-fluid">
@@ -116,17 +87,3 @@
         <!-- Back to Top -->
         <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
 
-
-        <!-- JavaScript Libraries -->
-        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
-        <script src="{{asset('lib/easing/easing.min.js')}}"></script>
-        <script src="{{asset('lib/slick/slick.min.js')}}"></script>
-
-
-
-        <!-- Template Javascript -->
-        <!-- <script src="js/main.js"></script> -->
-        <script src="{{asset('js/main.js')}}"></script>
-    </body>
-</html>
