b18169d72851b80e7d00a2cc0290738350fbe607
mani4578 <hunter.alone444@gmail.com>
2021-08-28 14:13:41 +0500

    login functionality

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,104 @@
-@extends('layouts.app')
+@extends('index')
+@section('css')
+    <link href="{{asset('css/login.css')}} "rel="stylesheet">
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <br> <br> <br>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    <div id="page-container">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+        <!-- Main Container -->
+        <main id="main-container">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+            <!-- Page Content -->
+            <!-- <div class="bg-image" style="background-image: url('assets/media/photos/photo6@2x.jpg');"> -->
+                <div class="hero-static bg-white-95">
+                    <div class="content">
+                        <div class="row justify-content-center">
+                            <div class="col-md-8 col-lg-6 col-xl-4">
+                                <!-- Sign In Block -->
+                                <div class="block block-themed block-fx-shadow  mb-0">
+                                    <div class="block-header">
+                                        <h3 class="block-title">Sign In</h3>
+                                        <div class="block-options">
+                                            <a class="btn-block-option font-size-sm" href="op_auth_reminder.html">Forgot Password?</a>
+                                            <a class="btn-block-option" href="op_auth_signup.html" data-toggle="tooltip" data-placement="left" title="New Account">
+                                                <i class="fa fa-user-plus"></i>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="block-content">
+                                        <div class="p-sm-3 px-lg-4 py-lg-5">
+                                            <h1 class="mb-2">World News</h1>
+                                            <p>Welcome, please login.</p>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+                                            <!-- Sign In Form -->
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                                            <form class="js-validation-signin" action="{{ route('login') }}" method="POST">
+                                                @csrf
+                                                <div class="py-3">
+                                                    <div class="form-group">
+                                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                                        {{-- <input type="text" class="form-control form-control-alt form-control-lg" id="login-username" name="login-username" placeholder="Username"> --}}
+                                                        @error('email')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                        @enderror
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                                        @error('password')
+                                                            <span class="invalid-feedback" role="alert">
+                                                                <strong>{{ $message }}</strong>
+                                                            </span>
+                                                        @enderror
+                                                        {{-- <input type="password" class="form-control form-control-alt form-control-lg" id="login-password" name="login-password" placeholder="Password"> --}}
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <div class="custom-control custom-checkbox">
+                                                            <input type="checkbox" class="custom-control-input" id="login-remember" name="login-remember">
+                                                            <label class="custom-control-label font-w400" for="login-remember">Remember Me</label>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group row">
+                                                    <div class="col-md-6 col-xl-5">
+                                                        <button type="submit" class="btn btn-primary">
+                                                            {{ __('Login') }}
+                                                        </button>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+                                                        @if (Route::has('password.request'))
+                                                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                                                {{ __('Forgot Your Password?') }}
+                                                            </a>
+                                                        @endif
+                                                        {{-- <button type="submit" class="btn btn-block btn-success">
+                                                            <i class="fa fa-fw fa-sign-in-alt mr-1"></i> Sign In
+                                                        </button> --}}
+                                                    </div>
+                                                </div>
+                                            </form>
+                                            <!-- END Sign In Form -->
+                                        </div>
+                                    </div>
                                 </div>
+                                <!-- END Sign In Block -->
                             </div>
                         </div>
+                    </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
                 </div>
-            </div>
-        </div>
+            <!-- </div> -->
+            <!-- END Page Content -->
+
+        </main>
+        <!-- END Main Container -->
     </div>
-</div>
+@endsection
+
+@section('js')
+        <script src="{{asset('js/plugins/jquery-validation/jquery.validate.min.js')}}"></script>
+        <script src="{{asset('js/js/pages/op_auth_signin.min.js')}}"></script>
 @endsection
