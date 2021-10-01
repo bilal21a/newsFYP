fedcb6e9fb5c7a807a9c8c32d099e9bd86ac1413
mani4578 <hunter.alone444@gmail.com>
2021-10-01 17:16:16 +0500

    admin login page

--- resources/views/auth/admin/adminlogin.blade.php
+++ resources/views/auth/admin/adminlogin.blade.php
@@ -1,102 +1,64 @@
 @extends('index')
 @section('css')
     <link href="{{asset('css/login.css')}} "rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href={{ asset('css/util.css') }}>
+	<link rel="stylesheet" type="text/css" href={{ asset('css/main.css') }}>
+	<link rel="icon" type="image/png" href={{ asset('images/icons/favicon.ico') }}>
+	<link rel="stylesheet" type="text/css" href={{ asset('vendor/bootstrap/css/bootstrap.min.css') }}>
+	<link rel="stylesheet" type="text/css" href={{ asset('vendor/animate/animate.css') }}>
+	<link rel="stylesheet" type="text/css" href={{ asset('vendor/css-hamburgers/hamburgers.min.css') }}>
+	<link rel="stylesheet" type="text/css" href={{ asset('vendor/animsition/css/animsition.min.css') }}>
+	<link rel="stylesheet" type="text/css" href={{ asset('vendor/select2/select2.min.css') }}>
+	<link rel="stylesheet" type="text/css" href={{ asset('vendor/daterangepicker/daterangepicker.css') }}>
+	<link rel="stylesheet" type="text/css" href={{ asset('fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}>
+	<link rel="stylesheet" type="text/css" href={{ asset('fonts/Linearicons-Free-v1.0.0/icon-font.min.css') }}>
+
 @endsection
 
+
 @section('content')
-
-
-    <div id="page-container">
-
-        <!-- Main Container -->
-        <main id="main-container">
-
-            <!-- Page Content -->
-            <!-- <div class="bg-image" style="background-image: url('assets/media/photos/photo6@2x.jpg');"> -->
-                <div class="hero-static bg-white-95">
-                    <div class="content">
-                        <div class="row justify-content-center">
-                            <div class="col-md-8 col-lg-6 col-xl-4">
-                                <!-- Sign In Block -->
-                                <div class="block block-themed block-fx-shadow  mb-0">
-                                    <div class="block-header">
-                                        <h3 class="block-title">Sign In</h3>
-                                        <div class="block-options">
-                                            @if (Route::has('password.request'))
-                                            <a class="btn-block-option font-size-sm" href="{{ route('password.request') }}">{{ __('Forgot Password?') }}</a>
-
-                                                            {{-- <a class="btn btn-link" href="{{ route('password.request') }}">
-                                                                {{ __('Forgot Password?') }}
-                                                            </a> --}}
-                                            @endif
-                                            {{-- <a class="btn-block-option font-size-sm" href="op_auth_reminder.html">Forgot Password?</a> --}}
-                                            <a class="btn-block-option" href="{{ route('register') }}" data-toggle="tooltip" data-placement="left" title="New Account">
-                                                <i class="fa fa-user-plus"></i>
-                                            </a>
-                                        </div>
-                                    </div>
-                                    <div class="block-content">
-                                        <div class="p-sm-3 px-lg-4 py-lg-5">
-                                            <h1 class="mb-2">World News</h1>
-                                            <p>Welcome, please login.</p>
-
-                                            <!-- Sign In Form -->
-
-                                            <form class="js-validation-signin" action="{{ route('admin.login') }}" method="POST">
-                                                @csrf
-                                                <div class="py-3">
-                                                    <div class="form-group">
-                                                        <input id="email" type="email" class="form-control form-control-alt form-control-lg @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Email">
-                                                        {{-- <input type="text" class="form-control form-control-alt form-control-lg" id="login-username" name="login-username" placeholder="Username"> --}}
-                                                        @error('email')
-                                                        <span class="invalid-feedback" role="alert">
-                                                            <strong>{{ $message }}</strong>
-                                                        </span>
-                                                        @enderror
-                                                    </div>
-                                                    <div class="form-group">
-                                                        <input id="password" type="password" class="form-control form-control-alt form-control-lg @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Password">
-                                                        @error('password')
-                                                            <span class="invalid-feedback" role="alert">
-                                                                <strong>{{ $message }}</strong>
-                                                            </span>
-                                                        @enderror
-                                                        {{-- <input type="password" class="form-control form-control-alt form-control-lg" id="login-password" name="login-password" placeholder="Password"> --}}
-                                                    </div>
-                                                    <div class="form-group">
-                                                        <div class="custom-control custom-checkbox">
-                                                            <input type="checkbox" class="custom-control-input" id="login-remember" name="login-remember" {{ old('remember') ? 'checked' : '' }}>
-                                                            <label class="custom-control-label font-w400" for="login-remember">{{ __('Remember Me') }}</label>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="form-group row">
-                                                    <div class="col-md-6 col-xl-5">
-                                                        <button type="submit" class="btn btn-block btn-success">
-                                                            <i class="fa fa-fw fa-sign-in-alt mr-1"></i> {{ __('Login') }}
-                                                        </button>
-                                                    </div>
-                                                </div>
-                                            </form>
-                                            <!-- END Sign In Form -->
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- END Sign In Block -->
-                            </div>
-                        </div>
-                    </div>
-
+<div class="limiter">
+    <div class="container-login100" style="background-image: url({{ URL::asset('images/bg-01.jpg') }})
+    /* {{ asset('images/bg-01.jpg') }};"> */
+        <div class="wrap-login100 p-t-30 p-b-50">
+            <span class="login100-form-title p-b-41">
+                Admin Login
+            </span>
+            <form class="login100-form validate-form p-b-33 p-t-5" action="{{ route('admin.login') }}" method="POST">
+                @csrf
+                <div class="wrap-input100 validate-input" data-validate = "Enter username">
+                    <input class="input100" type="email" name="email" placeholder="Email">
+                    <span class="focus-input100" data-placeholder="&#xe82a;"></span>
                 </div>
-            <!-- </div> -->
-            <!-- END Page Content -->
 
-        </main>
-        <!-- END Main Container -->
+                <div class="wrap-input100 validate-input" data-validate="Enter password">
+                    <input class="input100" type="password" name="password" placeholder="Password">
+                    <span class="focus-input100" data-placeholder="&#xe80f;"></span>
+                </div>
+
+                <div class="container-login100-form-btn m-t-32">
+                    <button class="login100-form-btn">
+                        Login
+                    </button>
+                </div>
+
+            </form>
+        </div>
     </div>
+</div>
+<div id="dropDownSelect1"></div>
 @endsection
 
 @section('js')
         <script src="{{asset('js/plugins/jquery-validation/jquery.validate.min.js')}}"></script>
         <script src="{{asset('js/js/pages/op_auth_signin.min.js')}}"></script>
+        <script src={{ asset('vendor/jquery/jquery-3.2.1.min.js') }}></script>
+        <script src={{ asset('vendor/animsition/js/animsition.min.js') }}></script>
+        <script src={{ asset('vendor/bootstrap/js/popper.js') }}></script>
+        <script src={{ asset('vendor/bootstrap/js/bootstrap.min.js') }}></script>
+        <script src={{ asset('vendor/select2/select2.min.js') }}></script>
+        <script src={{ asset('vendor/daterangepicker/moment.min.js') }}></script>
+        <script src="{{ asset('vendor/daterangepicker/daterangepicker.js') }}"></script>
+        <script src={{ asset('vendor/countdowntime/countdowntime.js') }}></script>
+        <script src="{{ asset('js/main.js') }}"></script>
 @endsection
