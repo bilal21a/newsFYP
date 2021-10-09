149ba97598ca657f442dcc54bdbfaa9fe5f9a705
mani4578 <hunter.alone444@gmail.com>
2021-10-09 15:11:02 +0500

    aa

--- resources/views/user/unicategories.blade.php
+++ resources/views/user/unicategories.blade.php
@@ -49,7 +49,7 @@
             </div>
             @endforeach
             @else
-                 <div class="container"><h5><centre>There is no Post for this Category</centre></h5></div>
+                 <div class="container" style="text-align: center"><h5>There is no Post for this Category</h5></div>
              @endif
          </div>
          {{ $posts_all->links() }}
